9943c0f79972aa3f8a426712091e59b378708aa2
itsash-droid <76574885+itsash-droid@users.noreply.github.com>
2021-01-10 17:11:12 -0800

    Add files via upload

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -12,32 +12,26 @@ public class MergeSort {
 
     private void merge (Comparable[] a, Comparable[] aux, int low, int mid, int high) {
 
-        int k = low;
-        int i = low, j = mid+1;
-        for (; i<=mid && j<=high;) {
-            if (less(a[j], a[i])) {
-                aux[k++] = a[j++];
-            } else {
-                aux[k++] = a[i++];
-            }
+        for (int k = low; k<=high; k++){
+            aux[k] = a[k];
         }
 
-        while (i<=mid) {
-            aux[k++] = a[i++];
-        }
 
-        while (j<high) {
-            aux[k++] = a[j++];
+        int i = low, j = mid + 1;
+        for (int k = low; k <= high; k++) {
+            if (i>mid) a[k] = aux[j++];
+            else if (j>high) a[k] = aux[i++];
+            else if (less(aux[i], aux[j])) a[k] = a[i++];
+            else a[k] = a[j++];
         }
     }
 
     private void mergeSort(Comparable[] a, Comparable[] aux, int start, int end) {
-        if (start < end) {
-            int mid = start + (end - start)/2;
-            mergeSort(a, aux, start, mid);
-            mergeSort(a, aux, mid + 1, end);
-            merge(a, aux, start, mid, end);
-        }
+        if (end <= start) return;
+        int mid = start + (end - start)/2;
+        mergeSort(a, aux, start, mid);
+        mergeSort(a, aux, mid + 1, end);
+        merge(a, aux, start, mid, end);
     }
 
     public void sort(Comparable[] a) {
@@ -45,12 +39,23 @@ public class MergeSort {
         mergeSort(a, aux, 0, a.length - 1);
     }
 
+    private void print(Comparable[] arr) {
+        for (Comparable integer : arr) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+    }
+
     public static void main (String args[]) {
         MergeSort ms = new MergeSort();
-        ms.sort(new Integer[] {2,5,6,7,8,1,3,4,7});
-
-        for (Comparable integer : ms.aux) {
-            System.out.println(integer);
-        }
+        Integer[] a = new Integer[] {2,5,6,7,8,1,3,4,7};
+        ms.sort(a);
+        ms.print(a);
+        a = new Integer[] {1,2,3,4,5,6,7,8,9};
+        ms.sort(a);
+        ms.print(a);
+        a = new Integer[] {9,8,7,6,5,4,3,2,1};
+        ms.sort(a);
+        ms.print(a);
     }
 }
